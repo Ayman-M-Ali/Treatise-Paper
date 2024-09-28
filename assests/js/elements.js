2f99fae264b1975c46b13aee387daabf32a828b2
Ayman-M-Ali <ayman.mohmd.ali@gmail.com>
2024-09-28 23:35:48 +0300

    Updated styles

---
+++ assests/js/elements.js
@@ -0,0 +1,12 @@
+export let numbers = document.querySelectorAll('.statistics__container .statistics__box');
+export let section = document.querySelector('.statistics');
+
+export const form = document.querySelector('.contact__form');
+export const theName = document.querySelector('.input-name');
+export const email = document.querySelector('.input-email');
+export const phone = document.querySelector('.input-phone');
+export const country = document.querySelector('.input-country');
+export const textArea = document.querySelector('.input-area');
+export const errorDiv = document.querySelector('.show-error');
+
+export const sectionElements = document.querySelectorAll('.section');
